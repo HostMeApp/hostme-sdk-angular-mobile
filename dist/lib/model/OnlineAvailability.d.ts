b6fcd15f4b95fda8f85340a2adbf9904478b85cb
Evgeny Popov <evgeny.popov@gmail.com>
2019-01-18 21:23:16 +0300

    Update OnlineAvailability.d.ts

--- dist/lib/model/OnlineAvailability.d.ts
+++ dist/lib/model/OnlineAvailability.d.ts
@@ -4,6 +4,7 @@ export interface OnlineAvailability {
     open?: boolean;
     requestedTime?: boolean;
     time?: string;
+    amount?: number;
 }
 export declare namespace OnlineAvailability {
     enum AvailabilityLevelEnum {
